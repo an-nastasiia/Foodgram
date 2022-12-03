ab07af06ab6ac2f0e6b4c7a5d8f0941b6c714755
Anastasiia Antipina <ananastasiia13@gmail.com>
2022-12-03 19:47:29 +0400

    Add str() to settings.py

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -7,11 +7,11 @@ load_dotenv()
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
-SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = str(os.getenv('SECRET_KEY'))
 
 DEBUG = os.getenv('DEBUG', default=False)
 
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS').split(' ')
+ALLOWED_HOSTS = str(os.getenv('ALLOWED_HOSTS')).split(' ')
 
 
 INSTALLED_APPS = [
